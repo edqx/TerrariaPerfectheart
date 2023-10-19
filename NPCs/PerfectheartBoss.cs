e7bca68034a83aea9e3cc07d1a453bb25b726f38
Edward Smale <essmale2005@gmail.com>
2023-10-19 23:44:10 +0100

    Remove happiness button, make perfectheart ascend into the air

--- NPCs/PerfectheartBoss.cs
+++ NPCs/PerfectheartBoss.cs
@@ -13,7 +13,15 @@ namespace PerfectheartMod.NPCs
 {
     public class PerfectheartBoss : ModNPC
     {
+        public enum FightStage {
+            WaitingForFight,
+            FightStarting,
+            PhaseOne
+        }
+
         public int NumFightAttempts = 0;
+        public FightStage Stage;
+        public float ascendVelocity = 0f;
 
         public override void SetStaticDefaults()
         {
@@ -35,7 +43,11 @@ namespace PerfectheartMod.NPCs
             Entity.dontTakeDamage = true;
             Entity.friendly = true;
             Entity.HitSound = SoundID.NPCHit5;
-            Entity.
+            Entity.despawnEncouraged = false;
+
+            Stage = FightStage.WaitingForFight;
+
+            NPCID.Sets.NoTownNPCHappiness[Entity.type] = true;
 
             Music = MusicLoader.GetMusicSlot(Mod, "Sounds/OMORI OST 130 Teehee Time");
         }
@@ -52,7 +64,8 @@ namespace PerfectheartMod.NPCs
         }
 
         public string GetChatForFightAttempt() {
-            switch (NumFightAttempts) {
+            switch (NumFightAttempts)
+            {
             case 1:
                 return Language.GetTextValue("Mods.PerfectheartMod.Dialogue.FightAttempt1");
             case 2:
@@ -70,24 +83,48 @@ namespace PerfectheartMod.NPCs
 
         public override void OnChatButtonClicked(bool firstButton, ref string shopName)
         {
-            if (firstButton) {
-                if (NumFightAttempts < 3) {
+            if (firstButton)
+            {
+                if (NumFightAttempts < 3)
+                {
                     NumFightAttempts++;
                     Main.npcChatText = GetChatForFightAttempt();
                 }
                 else
                 {
                     Main.CloseNPCChatOrSign();
-                    ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(Language.GetTextValue("Mods.PerfectheartMod.Dialogue.FightBegin")), Microsoft.Xna.Framework.Color.Pink);
+					if (Main.netMode == NetmodeID.Server)
+                    {
+                        ChatHelper.BroadcastChatMessage(NetworkText.FromKey("Mods.PerfectheartMod.Dialogue.FightBegin"), Microsoft.Xna.Framework.Color.Pink);
+					}
+					else if (Main.netMode == NetmodeID.SinglePlayer)
+                    {
+						Main.NewText(Language.GetTextValue("Mods.PerfectheartMod.Dialogue.FightBegin"), Microsoft.Xna.Framework.Color.Pink);
+					}
+                    Stage = FightStage.FightStarting;
                 }
             }
         }
 
         public override void AI()
         {
-			if (Entity.target < 0 || Entity.target == 255 || Main.player[Entity.target].dead || !Main.player[Entity.target].active) {
+			if (Entity.target < 0 || Entity.target == 255 || Main.player[Entity.target].dead || !Main.player[Entity.target].active)
+            {
 				Entity.TargetClosest();
 			}
+
+            if (Stage == FightStage.FightStarting) {
+                Entity.position = Entity.position - new Vector2(0f, ascendVelocity);
+                ascendVelocity += 0.4f;
+                if (ascendVelocity > 16f) {
+                    ascendVelocity = 16f;
+                }
+                if (Entity.position.Y < 0) {
+                    Stage = FightStage.PhaseOne;
+                }
+                Mod.Logger.Info($"Position ({Entity.position.X}, {Entity.position.Y})");
+                return;
+            }
 
             // Entity.position = Main.player[Entity.target].position + new Microsoft.Xna.Framework.Vector2(250f, -50f);
         }
